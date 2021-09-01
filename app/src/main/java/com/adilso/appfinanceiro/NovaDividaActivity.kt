40460aff7df9f6785348d03d8f2f87a3f802ec62
AdilsoAlt <adilso.alteia.jr@gmail.com>
2021-08-31 21:03:03 -0300

    Nova Divida Pronto

--- app/src/main/java/com/adilso/appfinanceiro/NovaDividaActivity.kt
+++ app/src/main/java/com/adilso/appfinanceiro/NovaDividaActivity.kt
@@ -1,8 +1,9 @@
 package com.adilso.appfinanceiro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.adilso.appfinanceiro.databinding.ActivityMainBinding
+import android.view.View
 import com.adilso.appfinanceiro.databinding.ActivityNovaDividaBinding
 
 class NovaDividaActivity : AppCompatActivity() {
@@ -11,5 +12,16 @@ class NovaDividaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityNovaDividaBinding = ActivityNovaDividaBinding.inflate(layoutInflater)
         setContentView(activityNovaDividaBinding.root)
+    }
+
+    fun onClick(view: View){
+        when(view){
+            activityNovaDividaBinding.btnVoltar -> {
+                startActivity(Intent(this, DashboardActivity::class.java))
+            }
+            activityNovaDividaBinding.btnSalvarDivida -> {
+                startActivity(Intent(this, DashboardActivity::class.java))
+            }
+        }
     }
 }
